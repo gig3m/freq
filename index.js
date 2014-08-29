830452b09c87241abc1276f6975a75c4a006d536
Kyle Arrington <kyle@arrington.co>
2014-08-28 23:42:03 -0500

    Refactored to npm module

---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict'
+
+var _ = require('lodash');
+var async = require('async');
+
+module.exports = function(strings) {
+	var dictionary = [];
+	async.each(strings, function(string, callback) {
+		var total;
+		var i = 0;
+		string = string.toString();
+		var words = string
+			.replace(/[.,?!;:()“”"'\-0123456789]/g, " ")
+			.replace(/[\u2026]/g, " ") //ellipsis
+			.replace(/\s+/g, " ")
+			.toLowerCase()
+			.split(" ");
+
+		total = words.length;
+		_(words).forEach(function (w) {
+			i++;
+			// console.log(w, _.contains(dictionary, { 'word': w }), dictionary);
+			if (_.find(dictionary, { 'word': w }) != undefined) {
+				var entry = _.find(dictionary, { 'word': w });
+				entry.count = entry.count+1;
+			}else{
+				dictionary.push({ 'word': w, 'count': 1 });
+			}
+
+			if (i === total) { callback(); } // all words processed
+		});
+
+	}, function(err){
+		if( err ) {
+		  console.log('An error occured, exiting.');
+		  process.exit();
+		} else {
+			//process order of dictionary
+			dictionary = _.sortBy(dictionary, function(e) {
+				return e.count*-1;
+			});
+		}
+	});
+	return dictionary;
+}
